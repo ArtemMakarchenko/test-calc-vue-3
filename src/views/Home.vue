7b0e46de2bda20988befc005eb97f9f82577bc8e
Artem Makarchenko <artem1998makarchenko@gmail.com>
2021-10-14 18:31:54 +0300

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,14 @@
+<template>
+  <Calculation/>
+</template>
+
+<script>
+import Calculation from '@/components/Calculation.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Calculation
+  }
+}
+</script>
